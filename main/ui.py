d9faa3cd7f03240464995ed8db0fe5cc84247492
JustAnEric <73326807+WWEMGamer2@users.noreply.github.com>
2023-03-08 18:13:35 +1100

    Create ui.py

---
+++ main/ui.py
@@ -0,0 +1,17 @@
+import tkinter,time,webview,os
+
+tewi = tkinter.Tk()
+
+tewi.geometry("600x400")
+tewi.overrideredirect(True)
+
+l = tkinter.Label(tewi, text="We hope you enjoyed the Beta test of our app. See you again later!", font="Roboto")
+b = tkinter.Button(tewi, text="Cancel", command=tewi.destroy, font="Roboto")
+l.pack()
+b.pack()
+webview.create_window('RapidChat', 'https://rapidchat.ericplayzyt.repl.co')
+webview.start()
+
+
+print("By turning on WebView, you agree to the terms and conditions of our website.")
+tewi.mainloop()
